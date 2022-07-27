6158c42aa6b25417c81f1e4c89363cd315a5278d
j2504 <89173612+j2504@users.noreply.github.com>
2022-07-27 13:41:29 -0500

    Update processForm.java

--- ExpenseReimbursement/src/main/java/com/revature/expense/controller/processForm.java
+++ ExpenseReimbursement/src/main/java/com/revature/expense/controller/processForm.java
@@ -17,7 +17,7 @@ import com.revature.expense.model.Reimbursement;
 /**
  * Servlet implementation class processForm
  */
-public class processForm extends HttpServlet {
+public class ProcessForm extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
